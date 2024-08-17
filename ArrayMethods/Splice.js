948ebe8e5a2c8592c1cd2b09639a774dd551fe5e
Mizael Barros <mizael.montenegro@gmail.com>
2024-08-17 20:09:27 -0300

    17 de ago

--- ArrayMethods/Splice.js
+++ ArrayMethods/Splice.js
@@ -1,23 +1,37 @@
-const mySplice = (arr, index, end, ...values) => {
+const mySplice = (arr, index, remove, ...values) => {
 
-    let newArr = arr
-    
+    let startP = []
+    let endP = []
+    let finalArr = []
+
+    if(typeof remove !== "number") return console.log(arr)
+    for (let i = 0; i < index; i++) {
+        startP[i] = arr[i]
+        finalArr[i] = startP[i]
+    }
 
     for (let i = 0; i < values.length; i++) {
-        newArr[index + i] = values[i]
-        
-        if(end !== 0) newArr.length --
-        end --
-        
+        finalArr[index + i] = values[i]
     }
-    
 
-    console.log(newArr)
+    for (let i = 0; i < arr.length - index; i++) {
+        endP[i] = arr[index + i]
+        finalArr[index + values.length + i] = endP[i]
+    }
+
+    for (let i = remove; i > 0; i--) {
+        finalArr[index + i + values.length - 1] = finalArr[finalArr.length - 1]
+        finalArr.length--
+    }
+
+    arr = finalArr
+    console.log(arr)
+
 }
 
-const arrTest = [1, 2, 3, 4, 5]
+const arrTest = [1, 2, 3, 4, 5, 7, 8, 9]
 
-// arrTest.splice(2, 0, "teste")
+// arrTest.splice(2, 3, "fodase")
 // console.log(arrTest)
 
-mySplice(arrTest, 2, 0, "teste 2")
+mySplice(arrTest, 2, 4, "fodase", "fodase 2")
--- ArrayMethods/Unshift.js
+++ ArrayMethods/Unshift.js
@@ -1,22 +1,23 @@
-const myUnshift = (arr: any[], ...values: any[]) => {
+const myUnshift = (arr, ...values) => {
 
-    const copy: any[] = [...values, ...arr]
-    for(let i = 0; i < copy.length; i++) {
-        arr[i] = copy[i]
-    }
+    // const copy: any[] = [...values, ...arr]
+    // for(let i = 0; i < copy.length; i++) {
+    //     arr[i] = copy[i]
+    // }
 
     
 
-    // for (let i = values.length; i > 0; i--) {
-    //     arr.length++
-    //     values[i] = values[i - 1]
-    //     for (let i = arr.length - 1; i >= 0; i--) {
-    //         arr[i] = arr[i - 1]
-    //     }
+    for (let i = values.length; i > 0; i--) {
+        arr.length++
+        values[i] = values[i - 1]
 
-    //     arr[0] = values[i]
+        for (let i = arr.length - 1; i >= 0; i--) {
+            arr[i] = arr[i - 1]
+        }
 
-    // }
+        arr[0] = values[i]
+
+    }
 
     console.log(arr)
 }
