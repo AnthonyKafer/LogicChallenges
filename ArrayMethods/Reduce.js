0a66459f2323655473822e2ab51466a044691b04
Anthony <anthony.vieira.lino@gmail.com>
2024-08-20 12:41:01 -0300

    20 de ago

--- ArrayMethods/Reduce.js
+++ ArrayMethods/Reduce.js
@@ -1,10 +1,7 @@
 const myReduce = (func, arr, initialValue) => {
   let acum;
-  if (!initialValue) initialValue = arr[0]
-
-  if (initialValue) {
-    acum = func(initialValue, arr[0]);
-  }
+  if (initialValue) acum = func(initialValue, arr[0]);
+  if (!initialValue) acum = arr[0];
 
   for (let i = 0; i < arr.length - 1; i++) {
     acum = func(acum, arr[i + 1]);
@@ -14,5 +11,5 @@ const myReduce = (func, arr, initialValue) => {
   console.log(arr);
 };
 
-const arrTest = [1, 2, 3, 4];
-myReduce((a, b) => a * b, arrTest, 5);
+const arrTest = [2, 3, 4, 5];
+myReduce((a, b) => a * b, arrTest);
